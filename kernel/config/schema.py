da49ba231f61c70d2b3b486474833c3b92615d3a
Alex M <makridenko.a@yandex.ru>
2021-05-21 15:04:34 +0300

    almost done with backend

---
+++ kernel/config/schema.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from graphene import ObjectType, Schema
+
+
+class Query(ObjectType):
+    pass
+
+
+class Mutation(ObjectType):
+    pass
+
+
+schema = Schema(query=Query, mutation=Mutation)

097a8c4ac7933b68edb6b15dbe6e6c64056662fe
Alex M <makridenko.a@yandex.ru>
2021-05-25 15:20:18 +0300

    add production configs and scripts

--- helper.sh
+++ helper.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PROJECT=${PWD##*/}
+
 if [[ $1 = 'config' ]]; then
     cat ./kernel/.env.example >> ./kernel/.env
     echo "Don't forget to change your SECRET_KEY"
@@ -9,6 +11,28 @@ if [[ $1 = 'config' ]]; then
 fi
 
 if [[ $1 = 'gql' ]]; then
+
+    if [[ $2 = '-d' ]]; then
+        docker-compose up -d --build web
+        docker exec ${PROJECT}_web_1 ./manage.sh gql
+        docker-compose down
+    fi
+
     cp ./kernel/schema.json ./client/schema.json
+    exit 1
+fi
+
+if [[ $1 = 'build' ]]; then
+    docker-compose up -d --build webclient
+    docker exec ${PROJECT}_webclient_1 yarn install
+    docker exec ${PROJECT}_webclient_1 yarn relay
+    docker exec ${PROJECT}_webclient_1 yarn build
+    docker-compose down
+
+    rm -f client/schema.json
+    rm -rf kernel/webclient
+    cp -r client/build kernel/webclient
+    rm -rf client/build
+
     exit 1
 fi
--- kernel/entrypoint.sh
+++ kernel/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+./manage.sh migrate --no-input
+./manage.sh collectstatic --no-input
 
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
